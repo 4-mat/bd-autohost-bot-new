9d5781b9fe0c1ee138381de5a396d13e1522cd7e
4-mat <36202270+4-mat@users.noreply.github.com>
2026-08-06 23:37:23 -0400

    review: empty base

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { connect } from "./connection.js";
-import "./parser.js";
-import { loadGameData } from "./data/index.js";
-
-loadGameData();
-connect();
-console.log("Ice Kyubs v2 starting...");
--- startbot.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\palha\Documents\GitHub\bd-autohost-bot-new"
-bun run bot
